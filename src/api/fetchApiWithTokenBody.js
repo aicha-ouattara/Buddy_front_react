dfc72b6ddd5b87807fdf9dd251b18988337cfce9
huongmay-nguyenphuoc <huong-may.nguyen-phuoc@laplateforme.io>
2022-03-28 17:38:46 +0200

    fix: add patch method

--- src/api/fetchApiWithTokenBody.js
+++ src/api/fetchApiWithTokenBody.js
@@ -10,3 +10,16 @@ Authorization:'Bearer ' + token}, body }).then(res => {
       return res;
     }
   })
+
+export const PatchWithTokenBody = async (url, method, token, body) =>
+fetch(url, { method, headers: {"Content-Type": "application/merge-patch+json",
+Accept: "application/json",
+Authorization:'Bearer ' + token}, body }).then(res => {
+  console.log("patch with Token", res)
+    if (!res.ok) {
+      console.log("not okay")
+      throw new Error(res.status);
+    } else {
+      return res;
+    }
+  })
