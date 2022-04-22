56d3ad12c3b4215f60c9284bc14c8e0aa3ec43f1
May <71871990+huongmay-nguyenphuoc@users.noreply.github.com>
2022-04-22 11:35:53 +0200

    feedscreen fix

--- src/screen/Tab/FeedScreen.js
+++ src/screen/Tab/FeedScreen.js
@@ -1,55 +1,103 @@
-import React, { useState, useEffect, useContext } from "react";
-import { View, ScrollView, StyleSheet, TexT } from "react-native";
-import { genericFetch } from "../../api/fetchApi";
-import { genericFetchWithToken } from "../../api/fetchApiWithToken";
-import ContainerCityCarrousel from "../../components/ContainerCityCarrousel";
-import ContainerFeedExperience from "../../components/ContainerFeedExperience";
-import Loading from "../../components/Loading";
-import { API_URL } from "@env";
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import {
+  View,
+  ScrollView,
+  StyleSheet
+} from 'react-native';
+import {
+  genericFetch
+} from '../../api/fetchApi';
+import {
+  genericFetchWithToken
+} from '../../api/fetchApiWithToken';
+import ContainerCityCarrousel from '../../components/ContainerCityCarrousel';
+import ContainerFeedExperience from '../../components/ContainerFeedExperience';
+import Loading from '../../components/Loading';
+import {
+  API_URL
+} from '@env';
 import jwt_decode from "jwt-decode";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { GlobalContext } from "../../context/Provider";
-// selector import pour token
-import { authState } from "../../store/auth/selectors";
-import { useDispatch, useSelector } from "react-redux";
 
-function FeedScreen({ navigation }) {
+function FeedScreen({
+  navigation
+}) {
+
   const [userId, setUserId] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [experiences, setExperiences] = useState([]);
-  const { token } = useSelector(authState);
-
-  console.log("token", token);
+  const [token, setToken] = useState("");
 
 
-  return (
-    <View style={styles.mainBody}>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        experiences.length > 0 && (
-          <ScrollView>
-            <ContainerCityCarrousel
-              experiences={experiences}
-              navigation={navigation}
-            />
-            <ContainerFeedExperience
-              experiences={experiences}
-              userId={userId}
-              navigation={navigation}
-            />
-          </ScrollView>
-        )
-      )}
-    </View>
+  /*récupère token automatiquement */
+  const body = JSON.stringify({
+    "login": "test",
+    "password": "test"
+  })
+
+  useEffect(() => {
+    genericFetch(`${API_URL}/login`, 'POST', body)
+      .then(json => json.json())
+      .then(data => setToken(data.token))
+      .catch(error => console.error(error))
+  }, [])
+
+  useEffect(() => {
+    setIsLoading(true)
+    genericFetchWithToken(`${API_URL}/experiences?visible=true`, 'GET', token)
+      .then(json => json.json())
+      .then(data => setExperiences(data))
+      .catch(error => console.error(error))
+      .finally(() => setIsLoading(false))
+
+    token.length > 0 && setUserId(jwt_decode(token).id); //get user Id from Token
+  }, [token])
+
+  return ( <
+    View style = {
+      styles.mainBody
+    } >
+
+    {
+      isLoading ? < Loading / > :
+
+        experiences.length > 0 &&
+        <
+        ScrollView >
+        <
+        ContainerCityCarrousel experiences = {
+          experiences
+        }
+      navigation = {
+        navigation
+      }
+      /> <
+      ContainerFeedExperience experiences = {
+        experiences
+      }
+      userId = {
+        userId
+      }
+      navigation = {
+        navigation
+      }
+      /> <
+      /ScrollView>
+    }
+
+    <
+    /View>
   );
 }
 
 const styles = StyleSheet.create({
   mainBody: {
     flex: 1,
-    padding: 10,
-  },
+    padding: 10
+  }
 });
+
 
 export default FeedScreen;
