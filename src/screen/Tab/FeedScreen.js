eb07486da99f5557aba0f969877e51c6ad799beb
May <71871990+huongmay-nguyenphuoc@users.noreply.github.com>
2022-04-22 11:49:00 +0200

    cleanup

--- src/screen/Tab/FeedScreen.js
+++ src/screen/Tab/FeedScreen.js
@@ -1,48 +1,36 @@
-import React, {
-  useState,
-  useEffect
-} from 'react';
-import {
-  View,
-  ScrollView,
-  StyleSheet
-} from 'react-native';
-import {
-  genericFetch
-} from '../../api/fetchApi';
-import {
-  genericFetchWithToken
-} from '../../api/fetchApiWithToken';
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import { genericFetch } from '../../api/fetchApi';
+import { genericFetchWithToken } from '../../api/fetchApiWithToken';
 import ContainerCityCarrousel from '../../components/ContainerCityCarrousel';
 import ContainerFeedExperience from '../../components/ContainerFeedExperience';
 import Loading from '../../components/Loading';
-import {
-  API_URL
-} from '@env';
+import { API_URL } from '@env';
 import jwt_decode from "jwt-decode";
+import { authState } from "../../store/auth/selectors";
+import { useDispatch, useSelector } from "react-redux";
+import { logOut } from "../../store/auth/slice";
 
-function FeedScreen({
-  navigation
-}) {
+function FeedScreen({ navigation }) {
 
   const [userId, setUserId] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [experiences, setExperiences] = useState([]);
-  const [token, setToken] = useState("");
+  // const [token, setToken] = useState("");
+  const { token } = useSelector(authState);
 
+  // /*récupère token automatiquement */
+  // const body = JSON.stringify({
+  //   "login": "test",
+  //   "password": "test"
+  // })
 
-  /*récupère token automatiquement */
-  const body = JSON.stringify({
-    "login": "test",
-    "password": "test"
-  })
-
-  useEffect(() => {
-    genericFetch(`${API_URL}/login`, 'POST', body)
-      .then(json => json.json())
-      .then(data => setToken(data.token))
-      .catch(error => console.error(error))
-  }, [])
+  // useEffect(() => {
+  //   genericFetch(`${API_URL}/login`, 'POST', body)
+  //     .then(json => json.json())
+  //     .then(data => setToken(data.token))
+  //     .catch(error => console.error(error))
+  // }, [])
 
   useEffect(() => {
     setIsLoading(true)
@@ -55,40 +43,19 @@ function FeedScreen({
     token.length > 0 && setUserId(jwt_decode(token).id); //get user Id from Token
   }, [token])
 
-  return ( <
-    View style = {
-      styles.mainBody
-    } >
+  return (
+    <View style={styles.mainBody}>
 
-    {
-      isLoading ? < Loading / > :
+      {isLoading ? <Loading /> :
 
         experiences.length > 0 &&
-        <
-        ScrollView >
-        <
-        ContainerCityCarrousel experiences = {
-          experiences
-        }
-      navigation = {
-        navigation
+        <ScrollView>
+          <ContainerCityCarrousel experiences={experiences} navigation={navigation} />
+          <ContainerFeedExperience experiences={experiences} userId={userId} navigation={navigation} />
+        </ScrollView>
       }
-      /> <
-      ContainerFeedExperience experiences = {
-        experiences
-      }
-      userId = {
-        userId
-      }
-      navigation = {
-        navigation
-      }
-      /> <
-      /ScrollView>
-    }
 
-    <
-    /View>
+    </View>
   );
 }
 
