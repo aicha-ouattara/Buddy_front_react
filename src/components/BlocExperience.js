0035b1f4c522961244526a29072028f0798485e6
aichaouattara <aicha.ouattara@laplateforme.io>
2022-05-31 13:39:20 +0200

    images upload and display ok !

--- src/components/BlocExperience.js
+++ src/components/BlocExperience.js
@@ -1,101 +1,143 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
-import { Avatar } from 'react-native-paper';
-import { API_URL } from '@env';
-import Bucket from './Bucket';
-import ModalMessage from './ModalMessage';
-import { genericFetchWithToken } from '../api/fetchApiWithToken';
-import { genericFetchWithTokenBody } from '../api/fetchApiWithTokenBody';
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { Avatar } from "react-native-paper";
+import { API_URL } from "@env";
+import Bucket from "./Bucket";
+import ModalMessage from "./ModalMessage";
+import { genericFetchWithToken } from "../api/fetchApiWithToken";
+import { genericFetchWithTokenBody } from "../api/fetchApiWithTokenBody";
 import { authState } from "../store/auth/selectors";
 import { useSelector } from "react-redux";
 
-
-const BlocExperience = ({ experience, user, navigation, hasActions = false }) => {
-
+const BlocExperience = ({
+  experience,
+  user,
+  navigation,
+  hasActions = false,
+}) => {
   const [liked, setLiked] = useState(false);
-  const [interestId, setInterestId] = useState(0)
+  const [interestId, setInterestId] = useState(0);
   const [modalVisible, setModalVisible] = useState(false);
   const [modalType, setModalType] = useState("");
   const { token, idUser } = useSelector(authState);
 
   useEffect(() => {
-    experience?.interests && experience.interests.map(function (interest) { //set interest id and liked if it exists
-      if (interest.user == `/api/users/${idUser}`) {
-        setLiked(true)
-        setInterestId(interest.id)
-      } else {console.log(interest.user, idUser)}
-    })
-  }, [])
-
+    experience?.interests &&
+      experience.interests.map(function(interest) {
+        //set interest id and liked if it exists
+        if (interest.user == `/api/users/${idUser}`) {
+          setLiked(true);
+          setInterestId(interest.id);
+        } else {
+          console.log(interest.user, idUser);
+        }
+      });
+  }, []);
 
   const handleLike = (experience) => {
     const bodyInterest = JSON.stringify({
-      "plan": false,
-      "title": experience.title,
-      "experience": `api/experiences/${experience.id}`
-    })
+      plan: false,
+      title: experience.title,
+      experience: `api/experiences/${experience.id}`,
+    });
 
-    liked ? (
-      genericFetchWithToken(`${API_URL}/interests/${interestId}`, 'DELETE', token),
-      console.log(`unliked ${experience.id} - interest ${interestId} deleted - by user ${idUser}`),
-      setLiked(false),
-      setModalVisible(true),
-      setModalType("unliked"),
-      setTimeout(() => {
-        setModalVisible(false)
-      }, 1000)
-    ) : (
-      experience.user.id != idUser ? ( //if not your own experience
-        genericFetchWithTokenBody(`${API_URL}/interests`, 'POST', token, bodyInterest)
-          .then(json => json.json())
-          .then(data => {setInterestId(data.id), console.log(`liked ${experience.id} - interest ${data.id} created - by user ${idUser}`)})
-          .catch(error => console.error(error)),
+    liked
+      ? (genericFetchWithToken(
+          `${API_URL}/interests/${interestId}`,
+          "DELETE",
+          token
+        ),
+        console.log(
+          `unliked ${experience.id} - interest ${interestId} deleted - by user ${idUser}`
+        ),
+        setLiked(false),
+        setModalVisible(true),
+        setModalType("unliked"),
+        setTimeout(() => {
+          setModalVisible(false);
+        }, 1000))
+      : experience.user.id != idUser //if not your own experience
+      ? (genericFetchWithTokenBody(
+          `${API_URL}/interests`,
+          "POST",
+          token,
+          bodyInterest
+        )
+          .then((json) => json.json())
+          .then((data) => {
+            setInterestId(data.id),
+              console.log(
+                `liked ${experience.id} - interest ${data.id} created - by user ${idUser}`
+              );
+          })
+          .catch((error) => console.error(error)),
         setLiked(true),
         setModalVisible(true),
         setModalType("liked"),
         setTimeout(() => {
-          setModalVisible(false)
-        }, 1000)
-        ) : (
-        console.log('Cannot like your own experiences'),
+          setModalVisible(false);
+        }, 1000))
+      : (console.log("Cannot like your own experiences"),
         setModalVisible(true),
         setModalType("error"),
         setTimeout(() => {
-          setModalVisible(false)
-        }, 2000)
-      )
-    )
-  }
+          setModalVisible(false);
+        }, 2000));
+  };
+  const encodedBase64 = experience.image;
 
   return (
     <View style={styles.box}>
-
-      <TouchableOpacity style={styles.blocExperience} onPress={() => { navigation.navigate('Experience', { id: experience.id }) }}>
-        <Image style={styles.experiencePicture} source={require(`../../assets/${experience.image}`) ?? require(`../../assets/exemple_ville.jpeg`)} />
+      <TouchableOpacity
+        style={styles.blocExperience}
+        onPress={() => {
+          navigation.navigate("Experience", { id: experience.id });
+        }}
+      >
+        <Image
+          style={styles.experiencePicture}
+          source={
+            { uri: encodedBase64 } ?? require(`../../assets/exemple_ville.jpeg`)
+          }
+        />
         <View style={styles.blocText}>
-          <Text><Text style={{fontWeight: "bold"}}>{experience.title}</Text><Text> | </Text><Text style={{fontStyle: "italic"}}>{experience.location}</Text></Text>
-          <Text numberOfLines={3} >{experience.content}</Text>
+          <Text>
+            <Text style={{ fontWeight: "bold" }}>{experience.title}</Text>
+            <Text> | </Text>
+            <Text style={{ fontStyle: "italic" }}>{experience.location}</Text>
+          </Text>
+          <Text numberOfLines={3}>{experience.content}</Text>
         </View>
       </TouchableOpacity>
 
       {/* Actions line display only with props hasActions */}
-      {hasActions &&
+      {hasActions && (
         <View style={styles.blocActions}>
-          <TouchableOpacity onPress={() => { navigation.navigate('User', { id: user.id }) }}>
-            <Avatar.Image style={styles.avatar} size={24} color="white" source={require('../../assets/profil.png')} />
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("User", { id: user.id });
+            }}
+          >
+            <Avatar.Image
+              style={styles.avatar}
+              size={24}
+              color="white"
+              source={require("../../assets/profil.png")}
+            />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => { handleLike(experience) }} >
+          <TouchableOpacity
+            onPress={() => {
+              handleLike(experience);
+            }}
+          >
             <Bucket liked={liked} />
           </TouchableOpacity>
         </View>
-      }
-    {modalVisible && modalType && 
-      <ModalMessage modalType={modalType} />
-    }
-    
+      )}
+      {modalVisible && modalType && <ModalMessage modalType={modalType} />}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   experiencePicture: {
@@ -104,7 +146,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   avatar: {
-    backgroundColor: "white"
+    backgroundColor: "white",
   },
   blocExperience: {
     flex: 1,
@@ -119,21 +161,21 @@ const styles = StyleSheet.create({
     shadowColor: "grey",
     shadowOpacity: 0.25,
     shadowRadius: 6,
-    elevation: 5
+    elevation: 5,
   },
   blocText: {
     flex: 1,
     width: "100%",
     marginLeft: 10,
     marginRight: 10,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
   blocActions: {
     paddingLeft: 10,
     borderLeftWidth: 3,
     borderLeftColor: "#f14d53",
-    justifyContent: "space-between"
-  }, 
+    justifyContent: "space-between",
+  },
 });
 
 export default BlocExperience;
--- src/components/brouillon.js
+++ src/components/brouillon.js
@@ -1,109 +1,97 @@
- {/* {(experience.interests.length == 0 && (
-                  <TouchableOpacity onPress={() => {handleUnlike(interest)}} >
-                  <Image style={styles.heartLogo} source={require('../../assets/heart.png')} />
-                </TouchableOpacity>
-                   ) )} 
+//  {/* {(experience.interests.length == 0 && (
+//                   <TouchableOpacity onPress={() => {handleUnlike(interest)}} >
+//                   <Image style={styles.heartLogo} source={require('../../assets/heart.png')} />
+//                 </TouchableOpacity>
+//                    ) )}
 
-    
+//                {(  experience?.interests && experience.interests.map(function(interest) {
+//                    if(interest.user == userUri ) {
+//                      return  <>
+//                      <TouchableOpacity onPress={() => {handleUnlike(interest)}} >
+//                        <Image style={styles.noheartLogo} source={require('../../assets/heart.png')} />
+//                      </TouchableOpacity>
+//                        </>
+//                    }  else {
+//                     return  <>
+//                     <TouchableOpacity onPress={() => {handleUnlike(interest)}} >
+//                       <Image style={styles.heartLogo} source={require('../../assets/heart.png')} />
+//                     </TouchableOpacity>
+//                       </>
+//                    }        })
 
-               {(  experience?.interests && experience.interests.map(function(interest) {
-                   if(interest.user == userUri ) {
-                     return  <>
-                     <TouchableOpacity onPress={() => {handleUnlike(interest)}} >
-                       <Image style={styles.noheartLogo} source={require('../../assets/heart.png')} />
-                     </TouchableOpacity>
-                       </>
-                   }  else {
-                    return  <>
-                    <TouchableOpacity onPress={() => {handleUnlike(interest)}} >
-                      <Image style={styles.heartLogo} source={require('../../assets/heart.png')} />
-                    </TouchableOpacity>
-                      </>
-                   }        })
-                   
-                   )}             */}
+//                    )}             */}
 
+//           {/* <TouchableOpacity onPress={() => {handleUnlike(interest)}} >
+//                 <Image style={styles.noheartLogo} source={require('../../assets/heart.png')} />
+//                </TouchableOpacity> */}
 
+//           {/* {isLoading ? <Text> Loading ... </Text> :
+//             (
 
+//                 experience.interests && experience.interests.map(interest => {
 
+//                     interest.user == userUri && <Heart handleUnlike={handleUnlike} handleLike={handleLike}/>
 
+//               // experience.interests && experience.interests.map(interest => {
 
+//               //   interest.user == userUri && <Heart handleUnlike={handleUnlike} handleLike={handleLike}/>
+//             )})} */}
 
-          {/* <TouchableOpacity onPress={() => {handleUnlike(interest)}} >
-                <Image style={styles.noheartLogo} source={require('../../assets/heart.png')} />
-               </TouchableOpacity> */}
+//   useEffect(() => {
+//     const ids = loggedInUserInterests
+//     .map(interest => interest.experience)
+//     .map(interest => interest.replace('/api/experiences/', ''))
+//     .map(id => parseInt(id))
+//     setLoggedInUserInterestsIds(ids)
+//   }, [loggedInUserInterests])
 
+//   <View style={styles.firstpart}>
+//   <Image style={{ width: 25, height: 25 }}
+//     source={require('../../assets/profil.png')}
+//   />
+//   <Text onPress={() => { navigation.navigate('User', { id: experience.user.id }) }} >{experience.user.login}</Text>
+// </View>
 
-          {/* {isLoading ? <Text> Loading ... </Text> : 
-            (
-            
-                experience.interests && experience.interests.map(interest => {
+// <View>
+//   <Text style={{ fontSize: 12, textAlign: 'justify', paddingBottom: 10 }}>{experience.description}</Text>
+//   <Text style={{ paddingBottom: 10 }}>date création : {experience.created_at}</Text>
+//   <Text style={{ paddingBottom: 10 }}>date modification : {experience.updated_at}</Text>
+// </View>
 
-                    interest.user == userUri && <Heart handleUnlike={handleUnlike} handleLike={handleLike}/>
-                 
-              // experience.interests && experience.interests.map(interest => {
+// <View style={styles.secondpart}>
+//   <Text>
+//     <Image style={{ width: 25, height: 25 }}
+//       source={require('../../assets/localisation.png')}
+//     /> {experience.location}
+//   </Text>
 
-              //   interest.user == userUri && <Heart handleUnlike={handleUnlike} handleLike={handleLike}/>
-            )})} */}
+//   <Text>
+//     <Image style={{ width: 25, height: 25 }}
+//       source={require('../../assets/time.png')}
+//     /> {experience.duration}
+//   </Text>
 
+//   <Text>
+//     <Image style={{ width: 25, height: 25 }}
+//       source={require('../../assets/foule.png')}
+//     />{experience.spots}</Text>
 
+// </View>
 
-            
-  useEffect(() => {
-    const ids = loggedInUserInterests
-    .map(interest => interest.experience)
-    .map(interest => interest.replace('/api/experiences/', ''))
-    .map(id => parseInt(id))
-    setLoggedInUserInterestsIds(ids)
-  }, [loggedInUserInterests])
+// <View style={styles.thirdpart}>
+//   <Text>
+//     <Image style={{ width: 25, height: 25 }}
+//       source={require('../../assets/doubleheart.png')}
+//     />
+//     {experience.interests.length} intéréssés
+//   </Text>
 
-  <View style={styles.firstpart}>
-  <Image style={{ width: 25, height: 25 }}
-    source={require('../../assets/profil.png')}
-  />
-  <Text onPress={() => { navigation.navigate('User', { id: experience.user.id }) }} >{experience.user.login}</Text>
-</View>
+//   <Text>
+//     <Image style={{ width: 25, height: 25 }}
+//       source={require('../../assets/doubleheart.png')}
+//     />  To do now
+//   </Text>
 
-<View>
-  <Text style={{ fontSize: 12, textAlign: 'justify', paddingBottom: 10 }}>{experience.description}</Text>
-  <Text style={{ paddingBottom: 10 }}>date création : {experience.created_at}</Text>
-  <Text style={{ paddingBottom: 10 }}>date modification : {experience.updated_at}</Text>
-</View>
+//   <BucketForm />
 
-<View style={styles.secondpart}>
-  <Text>
-    <Image style={{ width: 25, height: 25 }}
-      source={require('../../assets/localisation.png')}
-    /> {experience.location}
-  </Text>
-
-  <Text>
-    <Image style={{ width: 25, height: 25 }}
-      source={require('../../assets/time.png')}
-    /> {experience.duration}
-  </Text>
-
-  <Text>
-    <Image style={{ width: 25, height: 25 }}
-      source={require('../../assets/foule.png')}
-    />{experience.spots}</Text>
-
-</View>
-
-<View style={styles.thirdpart}>
-  <Text>
-    <Image style={{ width: 25, height: 25 }}
-      source={require('../../assets/doubleheart.png')}
-    />
-    {experience.interests.length} intéréssés
-  </Text>
-
-  <Text>
-    <Image style={{ width: 25, height: 25 }}
-      source={require('../../assets/doubleheart.png')}
-    />  To do now
-  </Text>
-
-  <BucketForm />
-
-</View> 
+// </View>
