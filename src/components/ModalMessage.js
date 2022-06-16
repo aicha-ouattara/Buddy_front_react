f5c058d8d216e8bb81cd7a01441be4bc69107d45
May <71871990+huongmay-nguyenphuoc@users.noreply.github.com>
2022-06-16 17:10:55 +0200

    fix modal

--- src/components/ModalMessage.js
+++ src/components/ModalMessage.js
@@ -1,56 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Modal, Image } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Modal, Image } from "react-native";
 
-const ModalMessage = ({ modalType = '', modalVisible }) => {
-
-  const [imageUrl, setImageUrl] = useState('')
-  const [title, setTitle] = useState('')
-  const [text, setText] = useState('')
-
+const ModalMessage = ({ modalType = "", modalVisible }) => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
 
   const selectType = () => {
     switch (modalType) {
       case "error":
-        setImageUrl("../../assets/lost-compass.gif")
-        setTitle("OOPS !")
-        setText("Tu essayes d'ajouter une de tes propres expériences à ta bucket list...")
+        setImageUrl("../../assets/lost-compass.gif");
+        setTitle("OOPS !");
+        setText(
+          "Tu essayes d'ajouter une de tes propres expériences à ta bucket list..."
+        );
         break;
       case "missing":
-        setImageUrl("../../assets/lost-compass.gif")
-        setTitle("OOPS !")
-        setText("Tous les champs sont obligatoires.")
+        setImageUrl("../../assets/lost-compass.gif");
+        setTitle("OOPS !");
+        setText("Tous les champs sont obligatoires.");
         break;
       case "stop":
-        setImageUrl("../../assets/icons/stop.gif")
-        setTitle("OOPS !")
-        setText("Tu ne peux pas liker une expérience dans ta to do now.")
+        setImageUrl("../../assets/icons/stop.gif");
+        setTitle("OOPS !");
+        setText("Tu ne peux pas liker une expérience dans ta to do now.");
         break;
-        case "superliked":
-        setImageUrl("../../assets/icons/todonow.gif")
-        setTitle("YAY !")
-        setText("Expérience ajoutée à ta to do now !")
+      case "superliked":
+        setImageUrl("../../assets/icons/todonow.gif");
+        setTitle("YAY !");
+        setText("Expérience ajoutée à ta to do now !");
         break;
       case "unsuperlike":
-        setImageUrl("../../assets/icons/stop.gif")
-        setTitle("OOPS !")
-        setText("Cette expérience est déjà dans ta To Do Now. Tu ne peux pas la supprimer si facilement.")
+        setImageUrl("../../assets/icons/stop.gif");
+        setTitle("OOPS !");
+        setText(
+          "Cette expérience est déjà dans ta To Do Now. Tu ne peux pas la supprimer si facilement."
+        );
         break;
       case "liked":
-        setImageUrl("../../assets/liked.gif")
-        setTitle("YAY !")
-        setText("Expérience ajoutée à ta bucket list !")
+        setImageUrl("../../assets/liked.gif");
+        setTitle("YAY !");
+        setText("Expérience ajoutée à ta bucket list !");
         break;
       case "unliked":
-        setImageUrl("../../assets/unliked.gif")
-        setTitle("OH ...")
-        setText("Expérience supprimée de ta bucket list.")
+        setImageUrl("../../assets/unliked.gif");
+        setTitle("OH ...");
+        setText("Expérience supprimée de ta bucket list.");
         break;
     }
-  }
+  };
 
   useEffect(() => {
-    selectType()
-  }, [])
+    selectType();
+  }, []);
 
   return (
     <Modal
@@ -62,42 +64,45 @@ const ModalMessage = ({ modalType = '', modalVisible }) => {
       <View style={styles.container}>
         <View style={styles.modalView}>
           <View style={styles.image}>
-            {imageUrl ? <Image style={styles.icon} source={imageUrl} /> : null}
+            {imageUrl ? (
+              <Image style={styles.icon} source={{ uri: imageUrl }} />
+            ) : null}
           </View>
           <View style={styles.text}>
-            <Text style={{ fontWeight: 'bold', paddingBottom: 5 }}>{title}</Text>
+            <Text style={{ fontWeight: "bold", paddingBottom: 5 }}>
+              {title}
+            </Text>
             <Text>{text}</Text>
           </View>
         </View>
       </View>
     </Modal>
-  )
+  );
+};
 
-}
-
-export default ModalMessage
+export default ModalMessage;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     alignContent: "center",
     backgroundColor: "rgba(0,0,0,0.7)",
   },
   modalView: {
     margin: 20,
-    width: "70vw",
+    marginLeft: 10,
+    marginRight: 10,
     borderRadius: 10,
     justifyContent: "center",
     alignContent: "center",
     elevation: 5,
-    alignSelf: 'center',
+    alignSelf: "center",
     textAlign: "center",
     backgroundColor: "white",
-
   },
   image: {
-    padding: 20
+    padding: 20,
   },
   icon: {
     width: 72,
@@ -109,5 +114,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#f2f2f2",
     padding: 15,
     borderRadius: 10,
-  }
-})
+  },
+});
